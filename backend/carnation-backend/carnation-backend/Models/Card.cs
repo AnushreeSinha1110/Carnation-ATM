f1cf308d0fa0dcad1b3c5e02e033cba772600414
avirup <avirupbhattacharyya2001@yahoo.com>
2023-08-10 13:56:20 +0530

    Fixed bug

--- backend/carnation-backend/carnation-backend/Models/Card.cs
+++ backend/carnation-backend/carnation-backend/Models/Card.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace carnation_backend.Models
 {
     public class Card
     {
+        [Key]
         public int cnum { get; set; }
         public int cpin { get; set; }
-        public DateOnly exp { get; set; }
+        public DateTime exp { get; set; }
         [ForeignKey("Account")]
         public Guid aidFK { get; set; }
     }
