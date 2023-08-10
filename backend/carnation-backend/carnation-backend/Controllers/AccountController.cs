d092edaa6a7afc6a374f769aec92073c06ceb8ca
Gyanendra <srirajshukla@gmail.com>
2023-08-10 11:56:51 +0530

    add account controllers

---
+++ backend/carnation-backend/carnation-backend/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using carnation_backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace carnation_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly DatabaseApiDbContext dbContext;
+
+        public AccountController(DatabaseApiDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAccounts()
+        {
+            var accounts = dbContext.Accounts.ToList();
+            return Ok(accounts);
+        }
+    }
+}
