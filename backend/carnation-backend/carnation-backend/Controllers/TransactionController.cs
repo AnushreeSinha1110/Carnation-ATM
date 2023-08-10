f359bf9c96224b99c2bb31da472dd873fadd7b54
AnushreeSinha1110 <anushreesinha1110@gmail.com>
2023-08-10 11:58:07 +0530

    Added Transaction

---
+++ backend/carnation-backend/carnation-backend/Controllers/TransactionController.cs
@@ -0,0 +1,23 @@
+﻿using carnation_backend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace carnation_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : Controller
+    {
+        private readonly DatabaseApiDbContext dbContext;
+        public TransactionController(DatabaseApiDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetTransactions()
+        {
+            return Ok(dbContext.Transactions.ToList());
+        }
+
+    }
+}
--- backend/carnation-backend/carnation-backend/Models/Transaction.cs
+++ backend/carnation-backend/carnation-backend/Models/Transaction.cs
@@ -1,6 +1,19 @@
-﻿namespace carnation_backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace carnation_backend.Models
 {
     public class Transaction
     {
+        [Key]
+        public Guid Tid { get; set; }
+
+        [ForeignKey("Account")]
+        public Guid Aid { get; set; }
+
+        public DateTime Timestamp { get; set; }
+        public decimal Amount { get; set; }
+        public string Type { get; set; }
+
     }
 }
