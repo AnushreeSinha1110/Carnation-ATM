41dec1adbe60a14e4d39fd942f5239c9ac8ab0fa
ameyz312 <ameyadk312@gmail.com>
2023-08-14 14:22:04 +0530

    UI bootstrap changes

--- frontend/src/Pages/AdminLogin.jsx
+++ frontend/src/Pages/AdminLogin.jsx
@@ -21,31 +21,43 @@ import Form from 'react-bootstrap/Form';
 
 
 import React, { useState } from 'react';
+import { Container, Row, Col } from "react-bootstrap";
 
 function AdminLogin() {
 
 
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+        <Container>
+            <Row>
+                <Col>Welcome To Carnation Bank.</Col>
+            </Row>
+            <Row>
+                <Col></Col>
+                <Col sm={4}>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                <Link to={`/dashboard`}>Login</Link>
-            </Button>
-        </Form>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="I agree to sell my soul to you (Become a banker)" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            <Link to={`/dashboard`}>Login</Link>
+                        </Button>
+                    </Form>
+                </Col>
+                <Col></Col>
+            </Row>    
+        </Container>
     )
 
 }
--- frontend/src/Pages/ViewDetails.jsx
+++ frontend/src/Pages/ViewDetails.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import CustomerDetailRow from "../Components/CustomerDetailRow"
 import React from 'react';
 import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
+import { Container, Col } from "react-bootstrap";
 
 function ViewDetails(props) {
     const [data, setData] = useState([])
@@ -20,27 +21,34 @@ function ViewDetails(props) {
         fetchInfo();
         console.log("data is:" + data);
     }, [])
-    return (<div>
-        Hello from the other side
-        {props.id}
-        <MDBTable>
-            <MDBTableHead>
-                <tr>
-                    <th scope='col'>#</th>
-                    <th scope='col'>Name</th>
-                    <th scope='col'>Phone</th>
-                    <th scope='col'>Age</th>
-                    <th scope='col'>Address</th>
-                </tr>
-            </MDBTableHead>
-            <MDBTableBody>
-            {data.map((entry) => {
-            return <CustomerDetailRow key={entry.cid} entry={entry} />
-            })}
-            </MDBTableBody>
-        </MDBTable>
-
-    </div>)
+    return (
+    <Container>
+        <Col></Col>
+        <Col sm={10}>
+            <div>
+                Hello from the other side
+                {props.id}
+                <MDBTable>
+                    <MDBTableHead>
+                        <tr>
+                            <th scope='col'>#</th>
+                            <th scope='col'>Name</th>
+                            <th scope='col'>Phone</th>
+                            <th scope='col'>Age</th>
+                            <th scope='col'>Address</th>
+                        </tr>
+                    </MDBTableHead>
+                    <MDBTableBody>
+                    {data.map((entry) => {
+                    return <CustomerDetailRow key={entry.cid} entry={entry} />
+                    })}
+                    </MDBTableBody>
+                </MDBTable>
+            </div>
+        </Col>
+        <Col></Col>
+    </Container>
+    )
 
 }
 
