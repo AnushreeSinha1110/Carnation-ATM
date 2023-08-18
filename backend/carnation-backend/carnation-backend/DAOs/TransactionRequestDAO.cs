28ec9ebf1dc359ffb49eb585bece907e359fa8d0
AnushreeSinha1110 <anushreesinha1110@gmail.com>
2023-08-18 09:08:18 +0530

    Modification of type in transaction

--- backend/carnation-backend/carnation-backend/DAOs/TransactionRequestDAO.cs
+++ backend/carnation-backend/carnation-backend/DAOs/TransactionRequestDAO.cs
@@ -1,10 +1,12 @@
-﻿namespace carnation_backend.DAOs
+﻿using carnation_backend.Models;
+
+namespace carnation_backend.DAOs
 {
     public class TransactionRequestDAO
     {
         public Guid Aid { get; set; }
         public decimal Amount { get; set; }
-        public string Type { get; set; }
+        public TransactionType Type { get; set; }
     }
 
 }
