d3bc8b87fd4f633d73014b3328d838b9dcec264c
ameyz312 <ameyadk312@gmail.com>
2023-08-21 14:49:10 +0530

    sync

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -2,24 +2,6 @@ import { Outlet } from "react-router-dom";
 // import { Container, Nav, Navbar, NavLink, Row, Col, NavItem,  DropdownItem, UncontrolledDropdown, DropdownMenu, DropdownToggle, Form, Input, Button} from 'reactstrap';
 import { Container, Nav, Navbar } from "react-bootstrap";
 
-// function Dashboard() {
-//     return <div>
-//         <nav>
-//             <span id="view"><Link to={`/dashboard/view/1`}>View</Link></span>
-//             <span id="create"><Link to={`/dashboard/create`}>Create</Link></span>
-//             <span id="Add Account"><Link to={`/dashboard/addaccount`}>Add Account</Link></span>
-//             <span id="card"><Link to={`/dashboard/card`}>Card Details</Link> </span>
-//             <span id="cardView"><Link to={`/dashboard/viewCard/1`}>ViewCard Details</Link> </span>
-//             <span id="logout"><Link to={`/`}>Logout</Link></span>
-
-//         </nav>
-//         <div id="details">
-//             <Outlet />
-//         </div>
-
-//     </div>
-// }
-
 function Dashboard() {
   return (
     <div>
