fd9b1eef01d67fa7459343e9f2dd424a65b8f61a
Gyanendra <srirajshukla@gmail.com>
2023-08-22 15:15:37 +0530

    Added test for account creation, deletion, withdrawal

--- backend/carnation-backend/carnation-backend-test/RepositoryTest/AccountRepositoryTest.cs
+++ backend/carnation-backend/carnation-backend-test/RepositoryTest/AccountRepositoryTest.cs
@@ -1,11 +1,7 @@
-﻿using carnation_backend.Models;
+﻿using carnation_backend.DAOs;
+using carnation_backend.Models;
 using carnation_backend.Repository;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace carnation_backend_test.RepositoryTest
 {
@@ -13,6 +9,8 @@ namespace carnation_backend_test.RepositoryTest
     {
         private readonly Mock<IAccountRepository> accountRepository;
         private Guid UserId1 = Guid.NewGuid();
+        private Guid UserId2 = Guid.NewGuid();
+        private Guid UserId3 = Guid.NewGuid();
 
         public AccountRepositoryTest()
         {
@@ -21,7 +19,7 @@ namespace carnation_backend_test.RepositoryTest
 
         private List<Account> GetAccountsData()
         {
-           return new List<Account>()
+            return new List<Account>()
             {
                 new Account
                 {
@@ -38,6 +36,14 @@ namespace carnation_backend_test.RepositoryTest
                     AccountOwnerId = 1,
                     AType = (AccountType) 0,
                     Balance = 0,
+                },
+                new Account
+                {
+                    Id = UserId2,
+                    AccountNumber = Guid.NewGuid().ToString(),
+                    AccountOwnerId = 2,
+                    AType = (AccountType) 0,
+                    Balance = 100,
                 }
             };
         }
@@ -45,7 +51,15 @@ namespace carnation_backend_test.RepositoryTest
         [Fact]
         public void TestGetAccountByCID()
         {
+            var accountList = GetAccountsData();
+            accountRepository.Setup(x => x.GetByCid(1)).Returns(accountList.SkipLast(1).ToList());
 
+            var result = accountRepository.Object;
+            var accountResult = result.GetByCid(1).ToList();
+
+            Assert.NotNull(accountResult);
+            Assert.Equal(2, accountResult.Count);
+            Assert.Equal(accountList[0].AccountOwnerId, accountResult[0].AccountOwnerId);
         }
 
         [Fact]
@@ -78,18 +92,77 @@ namespace carnation_backend_test.RepositoryTest
         [Fact]
         public void TestCreateAccount()
         {
+            var account = new Account
+            {
+                Id = UserId3,
+                AccountNumber = Guid.NewGuid().ToString(),
+                AccountOwnerId = 3,
+                AType = (AccountType)1,
+                Balance = 500,
+            };
+
+            var accountList = GetAccountsData();
+            accountList.Add(account);
+
+            AccountDAO accountDAO = new AccountDAO
+            {
+                AccountOwnerId = 3,
+                AType = (AccountType)1,
+            };
+
+            Customer customer = new Customer
+            {
+                Id = 3
+            };
+
+            accountRepository.Setup(x => x.CreateAccount(accountDAO, customer)).Returns(account);
+
+            var result = accountRepository.Object;
+            var accountResult = result.CreateAccount(accountDAO, customer);
+
+            Assert.NotNull(accountResult);
+            Assert.Equal(account.Id, accountResult.Id);
+        }
+
+        [Fact]
+        public void TestDeleteAccountSuccess()
+        {
+            var accountList = GetAccountsData();
+
+            accountRepository.Setup(x => x.DeleteAccount(UserId3)).Returns(true);
+            var result = accountRepository.Object;
+            var accountResult = result.DeleteAccount(UserId3);
+
+            Assert.True(accountResult);
 
         }
 
         [Fact]
-        public void TestDeleteAccount()
+        public void TestDeleteAccountFailure()
         {
+            var accountList = GetAccountsData();
+
+            accountRepository.Setup(x => x.DeleteAccount(Guid.NewGuid())).Returns(false);
+            var result = accountRepository.Object;
+            var accountResult = result.DeleteAccount(Guid.NewGuid());
+
+            Assert.False(accountResult);
 
         }
-
         [Fact]
         public void TestUpdateBalanceWithdraw()
         {
+            var accountList = GetAccountsData();
+            var account = accountList[2];
+            account.Balance -= 50;
+
+            accountRepository.Setup(x => x.UpdateBalance(UserId2, 50, TransactionType.WITHDRAW)).Returns(account);
+
+            var result = accountRepository.Object;
+            var accountResult = result.UpdateBalance(UserId2, 50, TransactionType.WITHDRAW);
+
+            Assert.NotNull(accountResult);
+            Assert.Equal(50, accountResult.Balance);
 
         }
 
