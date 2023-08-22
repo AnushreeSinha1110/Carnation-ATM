83aa53f15763a1b286639b53f61f652bd0b71b16
Gyanendra <srirajshukla@gmail.com>
2023-08-22 12:02:55 +0530

    setup account repo test

---
+++ backend/carnation-backend/carnation-backend-test/RepositoryTest/AccountRepositoryTest.cs
@@ -0,0 +1,29 @@
+﻿using carnation_backend.Models;
+using carnation_backend.Repository;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carnation_backend_test.RepositoryTest
+{
+    public class AccountRepositoryTest
+    {
+        private readonly Mock<IAccountRepository> accountRepository;
+
+        public AccountRepositoryTest()
+        {
+            accountRepository = new Mock<IAccountRepository>();
+        }
+
+        [Fact]
+        public void TestAccountCreation()
+        {
+            var account = new Account();
+
+            Assert.NotNull(account);
+        }
+    }
+}
