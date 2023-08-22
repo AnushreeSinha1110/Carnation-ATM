4d3cfd8f987efbd9c2de2773ac6ff2be904d1dbd
avirup <avirupbhattacharyya2001@yahoo.com>
2023-08-22 13:44:26 +0530

    CustomerRepository Test

---
+++ backend/carnation-backend/CarnationTestProject/CustomerRepositoryUnitTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarnationTestProject
+{
+    public class CustomerRepositoryUnitTest
+    {
+        private readonly Mock<ICustomerRepository> customerRepository;
+        public ConsumerRepositoryUnitTest()
+        {
+            customerRepository=new Mock<ICustomerRepository> ();
+        }
+        [Fact]
+        public void Test_GetCustomerList()
+        {
+            Assert.Equal(true, true);
+        }
+    }
+}
