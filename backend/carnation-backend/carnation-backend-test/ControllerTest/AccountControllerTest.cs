92a782c2db786683f03c7aa751d516b088aa052e
Gyanendra <srirajshukla@gmail.com>
2023-08-23 11:42:51 +0530

    controller test setup

---
+++ backend/carnation-backend/carnation-backend-test/ControllerTest/AccountControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using carnation_backend.Controllers;
+using carnation_backend.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carnation_backend_test.ControllerTest
+{
+    public class AccountControllerTest
+    {
+        private readonly Mock<IAccountRepository> accountMockRepository;
+        private readonly Mock<ICustomerRepository> customerMockRepository;
+        private readonly AccountController accountController;
+
+        public AccountControllerTest()
+        {
+            this.accountMockRepository = new Mock<IAccountRepository>();
+            this.customerMockRepository = new Mock<ICustomerRepository>();
+            this.accountController = new AccountController(accountMockRepository.Object, customerMockRepository.Object);
+        }
+
+        [Fact]
+        public void GetAllTest()
+        {
+            var result = accountController.GetAccounts();
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
